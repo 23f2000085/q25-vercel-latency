54d194393870be601943794aeba297ef5d4540fa
23f2000085 <23f2000085@ds.study.iitm.ac.in>
2026-06-21 17:08:35 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
     expose_headers=["*"],
 )
